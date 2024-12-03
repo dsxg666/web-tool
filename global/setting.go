98b475a65bac659e42d97006e79d2d0039436bd1
dsxg666 <1873887255@qq.com>
2024-12-03 18:08:51 +0800

    add github login

--- global/setting.go
+++ global/setting.go
@@ -5,7 +5,8 @@ import "github.com/dsxg666/web-tool/pkg/setting"
 var (
 	ServerSetting   *setting.ServerSetting
 	AppSetting      *setting.AppSetting
-	DatabaseSetting *setting.DatabaseSetting
+	MySQLSetting    *setting.MySQLSetting
 	EmailSetting    *setting.EmailSetting
 	JwtTokenSetting *setting.JwtTokenSetting
+	RedisSetting    *setting.RedisSetting
 )
